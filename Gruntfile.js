5aecee9d3d390fe4864c97158ab15efd8561af27
Timmy Willison <timmywillisn@gmail.com>
2015-05-29 11:09:32 -0400

    Tests: update chosen as 1.0.0 no longer exists

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
 			},
 			images: {
 				options: {
-					srcPrefix: 'bower_modules/chosen/public',
+					srcPrefix: 'bower_modules/chosen',
 					destPrefix: 'tmp/images/chosen'
 				},
 				// Test source arrays
